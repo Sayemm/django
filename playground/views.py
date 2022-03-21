75ecd83ec8a0d8ac0f09befc76eb0129890c356f
Sayemm <sayemseer@gmail.com>
2022-03-22 00:10:39 +0600

    Starting ORM

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def calculate():
-    x = 1
-    y = 2
-    return x
-# Create your views here.
+
 def say_hello(request):
-    # return HttpResponse('Hello World')
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Sayemmmmm'})
+    return render(request, 'hello.html', {'name': 'Mosh'})
