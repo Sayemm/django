d5fca89499966bbaed48dfc513dc5c2718fd7f61
Sayemm <sayemseer@gmail.com>
2022-04-17 12:44:51 +0600

    commit

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,8 @@
 
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count, ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from store.models import Customer, Product
-
+from store.models import Order, Product
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-    query_set = Product.objects.annotate(
-        discounted_price = discounted_price
-    )
+    query_set =  Product.objects.raw('SELECT * FROM store_product')
 
     return render(request, 'hello.html', {'name': 'Sayem', 'result': list(query_set)})
